635c8652410e2519da05bd7c88597174687e282b
Iqbal Indra <iqbalolala@gmail.com>
2024-02-02 00:57:05 +0700

    Update post_deploy.sh

--- post_deploy.sh
+++ post_deploy.sh
@@ -2,9 +2,10 @@
 
 # update application cache
 php artisan optimize
+php artisan up
 
 # start the application
 
-service php8.1-fpm start &&  nginx -g "daemon off;"
+#service php8.1-fpm start &&  nginx -g "daemon off;"
 
 
